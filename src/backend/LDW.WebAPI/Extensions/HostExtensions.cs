18067c938bcb76044c00625e11525eb48151ae35
Olesia Tvardovska <tvardovska.29@gmail.com>
2020-11-28 13:41:20 +0200

    Add configs for api deployment

---
+++ src/backend/LDW.WebAPI/Extensions/HostExtensions.cs
@@ -0,0 +1,24 @@
+﻿using LDW.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LDW.WebAPI.Extensions
+{
+    public static class HostExtensions
+	{
+		public static IHost MigrateDatabase(this IHost webHost)
+		{
+			using (var scope = webHost.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+				var context = services.GetRequiredService<ApplicationDbContext>();
+				context.Database.Migrate();
+				var identityContext = services.GetRequiredService<UserDbContext>();
+				identityContext.Database.Migrate();
+			}
+
+			return webHost;
+		}
+	}
+}
