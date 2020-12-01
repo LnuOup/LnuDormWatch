7e3e6dca77c3841f6e1d5f452410ec661cffad35
Dmitry Afanasiev <dmitry.afanasiev@cigen.me>
2020-12-01 20:28:11 +0200

    Post-merge fixes

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -11,29 +11,23 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { HomeComponent } from './components/home/home.component';
 import { DormDetailComponent } from './components/dorm-detail/dorm-detail.component';
 import { ForumSectionThreadListComponent } from './components/forum-section-thread-list/forum-section-thread-list.component';
-<<<<<<< HEAD
 import { ForumThreadComponent } from './components/forum-thread/forum-thread.component';
 import { LoginComponent } from './components/login/login.component';
-=======
-import {AgmCoreModule} from "@agm/core";
+import {AgmCoreModule} from '@agm/core';
 import 'hammerjs';
 import 'mousetrap';
-import {GalleryModule} from "@ks89/angular-modal-gallery";
-
->>>>>>> origin/feature/dorms_list
+import {GalleryModule} from '@ks89/angular-modal-gallery';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    ReactiveFormsModule
-=======
+    ReactiveFormsModule,
     AgmCoreModule.forRoot({
       apiKey : 'AIzaSyAaZgGxzh87e9jXC1LOvz8zZJSMH3E77o0'
     }),
     GalleryModule.forRoot()
->>>>>>> origin/feature/dorms_list
   ],
   declarations: [
     AppComponent,
