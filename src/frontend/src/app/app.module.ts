8de5e81d3f4acc8f5254164b53e9b15527f57bd3
Roman Nakonechnyi <rnakonechnyi@exoft.net>
2020-12-16 23:24:38 +0200

    Adddd missing import

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { DormListComponent } from './components/dorm-list/dorm-list.component';
