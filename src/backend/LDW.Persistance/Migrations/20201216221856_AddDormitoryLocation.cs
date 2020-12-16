161ab999f58ceef74668042ed5afb3e278589ab6
Serhii Nykon <sergiy.nikon2014@gmail.com>
2020-12-17 00:21:21 +0200

    Implement dormitory location on backend

---
+++ src/backend/LDW.Persistance/Migrations/20201216221856_AddDormitoryLocation.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LDW.Persistence.Migrations
+{
+    public partial class AddDormitoryLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Dormitories",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Dormitories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Dormitories");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Dormitories");
+        }
+    }
+}
