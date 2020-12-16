57fe77a204dd13f973691ffbec872eae0840a7e4
Serhii Nykon <sergiy.nikon2014@gmail.com>
2020-12-16 23:51:08 +0200

    Implement dormitory location logic

--- src/backend/LDW.Application/Features/DormitoryFeatures/Commands/UpdateDormitoryCommand.cs
+++ src/backend/LDW.Application/Features/DormitoryFeatures/Commands/UpdateDormitoryCommand.cs
@@ -12,8 +12,10 @@ namespace LDW.Application.Features.DormitoryFeatures.Commands
         public int Number { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
 
-        public class UpdateDormitoryCommandHandler : IRequestHandler<UpdateDormitoryCommand, int>
+		public class UpdateDormitoryCommandHandler : IRequestHandler<UpdateDormitoryCommand, int>
         {
             private readonly IApplicationDbContext _context;
             public UpdateDormitoryCommandHandler(IApplicationDbContext context)
@@ -33,6 +35,8 @@ namespace LDW.Application.Features.DormitoryFeatures.Commands
                 dormitoryToUpdate.Number = request.Number;
                 dormitoryToUpdate.Address = request.Address;
                 dormitoryToUpdate.PhoneNumber = request.PhoneNumber;
+				dormitoryToUpdate.Latitude = request.Latitude;
+				dormitoryToUpdate.Longitude = request.Longitude;
 
                 await _context.SaveChangesAsync(cancellationToken: cancellationToken);
 
--- src/backend/LDW.Domain/Entities/DormitoryEntity.cs
+++ src/backend/LDW.Domain/Entities/DormitoryEntity.cs
@@ -1,5 +1,6 @@
 ﻿using LDW.Domain.Entities.Common;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace LDW.Domain.Entities
 {
@@ -8,6 +9,8 @@ namespace LDW.Domain.Entities
 		public int Number { get; set; }
 		public string Address { get; set; }
 		public string PhoneNumber { get; set; }
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
 
 		public ICollection<DormitoryPictureEntity> DormitoryPictures { get; set; }
 	}
